b49505fa865d614b95abc4771f4cedc4a6df57b1
LunkenMottagelse <torstein.holmengen@gmail.com>
2025-10-21 11:17:08 +0200

    invert image idk

--- tmu/clause_bank/clause_bank_pl.py
+++ tmu/clause_bank/clause_bank_pl.py
@@ -255,7 +255,7 @@ class ClauseBankPL(BaseClauseBank):
         literals = self.get_literals()
         modified_model = []
         for row in literals:
-            packed_row = self.pack_bits_32(row[::-1].tolist())
+            packed_row = self.pack_bits_32(row.tolist())
             modified_model.append(packed_row)
 
         modified_model = np.array(modified_model, dtype=object)
